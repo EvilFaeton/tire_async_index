8dc87a769af14e4adcb6726b30fa1a23795824c5
Sergey Efremov <efremov.sergey@gmail.com>
2013-06-25 12:57:20 +0400

    No ActiveSuport dependency

--- lib/tire/model/async_callbacks.rb
+++ lib/tire/model/async_callbacks.rb
@@ -2,42 +2,44 @@ module Tire
   module Model
     module AsyncCallbacks
 
-      extend ActiveSupport::Concern
-
       ID_CONVERSION = {
           'Moped::BSON::ObjectId' => :to_s
       }
 
-      included do
+      def self.included(base)
         # Bind after save or create callback
-        if self.respond_to? :after_commit
-          after_commit :__async_tire_save_index
+        if base.respond_to? :after_commit
+          base.send :after_commit, :async_tire_save_index
 
-        elsif self.respond_to? :after_save
-          after_save :__async_tire_save_index
+        elsif base.respond_to? :after_save
+          base.send :after_save, :async_tire_save_index
         end
 
         # Bind before destroy callback
-        if self.respond_to? :before_destroy
-          before_destroy :__async_tire_delete_index
+        if base.respond_to? :before_destroy
+          base.send :before_destroy, :async_tire_delete_index
         end
       end
 
-      def __async_tire_save_index
-        __async_tire_callback :update
+      private
+
+      def async_tire_save_index
+        async_tire_callback :update
       end
 
-      def __async_tire_delete_index
-        __async_tire_callback :delete
+      def async_tire_delete_index
+        async_tire_callback :delete
       end
 
-      def __async_tire_callback(type)
+      def async_tire_callback(type)
         case TireAsyncIndex.engine
           when :sidekiq
-            TireAsyncIndex::Workers::SidekiqUpdateIndex.perform_async type, self.class.name, __async_tire_object_id
+            TireAsyncIndex::Workers::SidekiqUpdateIndex.
+              perform_async type, self.class.name, async_tire_object_id
 
           when :resque
-            Resque.enqueue TireAsyncIndex::Workers::ResqueUpdateIndex, type, self.class.name, __async_tire_object_id
+            Resque.enqueue TireAsyncIndex::Workers::ResqueUpdateIndex,
+              type, self.class.name, async_tire_object_id
 
           else
             case type
@@ -49,7 +51,7 @@ module Tire
         end
       end
 
-      def __async_tire_object_id
+      def async_tire_object_id
         if (method = ID_CONVERSION[self.id.class.name])
           self.id.send(method)
         else
