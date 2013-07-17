5540079a6f6549a5e1c20e04f282d0eba5ca1c2b
we4tech <hasan83bd@gmail.com>
2013-07-17 16:50:41 +0600

    Removed Kernal.const_get because it works only on Ruby 2.x version. (ActiveSupport dependency is not an option) Added recursively constant lookup option so that it can load const from multiple namespace.

---
+++ spec/update_index_spec.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+require 'tire'
+require 'sidekiq'
+require 'resque'
+require 'tire_async_index'
+
+module Level1
+  module Level2
+    class Model
+    end
+  end
+end
+
+describe TireAsyncIndex::Workers::UpdateIndex do
+
+  describe '#perform' do
+    let(:instance) { described_class.new }
+
+    it 'resolves class constant' do
+      expect {
+        instance.perform(:nothing, 'Level1::Level2::Model', 123)
+      }.not_to raise_error
+    end
+  end
+
+end
