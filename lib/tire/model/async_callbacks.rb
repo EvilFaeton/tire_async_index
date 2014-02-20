0e29b345a80fe44ce1f901bab3db2e29e6ca780c
adrien <adrien.montfort@gmail.com>
2014-02-20 15:00:31 +0100

    Prevent to try and update the index on destroy for ActiveRecord

--- lib/tire/model/async_callbacks.rb
+++ lib/tire/model/async_callbacks.rb
@@ -9,7 +9,7 @@ module Tire
       def self.included(base)
         # Bind after save or create callback
         if base.respond_to? :after_commit
-          base.send :after_commit, :async_tire_save_index
+          base.send :after_commit, :async_tire_save_index, :if => :persisted?
 
         elsif base.respond_to? :after_save
           base.send :after_save, :async_tire_save_index
